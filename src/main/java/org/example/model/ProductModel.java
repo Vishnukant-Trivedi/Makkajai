c26e45e543ac2ca238d4028a9b8d0f2fb436646b
Vishnukant Trivedi <vishnu@Vishnukants-MacBook-Pro.local>
2024-12-20 00:11:19 +0530

    First commit

---
+++ src/main/java/org/example/model/ProductModel.java
@@ -0,0 +1,20 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProductModel {
+    @NotNull
+    private final String name;
+    @NotNull
+    private final BigDecimal price;
+    private final boolean isImported;
+    private final boolean isExempt;
+}
